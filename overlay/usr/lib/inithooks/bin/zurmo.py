d02edd5709f55163eab6ded3196ff73e7978f929
root <root@tkldev.(none)>
2015-04-09 05:25:18 +0000

    Fixed zurmo inithook (changepassword)

--- overlay/usr/lib/inithooks/bin/zurmo.py
+++ overlay/usr/lib/inithooks/bin/zurmo.py
@@ -67,8 +67,8 @@ def main():
     m.execute('UPDATE zurmo._user SET hash=\"%s\" WHERE username=\"admin\";' % hash)
     '''
     
-    zurmo = "~/public_html/zurmo/app/protected/commands/zurmoc.php"
-    system("php %s changePassword --username=super --password=%s" % (zurmo, password))
+    zurmo = "/var/www/zurmo/app/protected/commands/zurmoc.php"
+    system("cd %s;zurmoc changePassword --username=admin --password=%s" % (zurmo, password))
 
     conf = "/var/www/zurmo/app/protected/config/perInstance.php"
     system("sed -i \"s|hostInfo.*|hostInfo'] = 'http://%s';|\" %s" % (domain, conf))
